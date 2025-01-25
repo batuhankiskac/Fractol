66ee58a36085c1bcc1e375a14b7934c414b062f4
batuhankiskac <kiskacbatuhan@gmail.com>
2025-01-25 18:32:02 +0300

    Add event handling and cleanup functions for fractol

--- src/fractol_events.c
+++ src/fractol_events.c
@@ -1,20 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fractol_init.c                                     :+:      :+:    :+:   */
+/*   fractol_events.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bkiskac <bkiskac@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/25 18:27:01 by bkiskac           #+#    #+#             */
-/*   Updated: 2025/01/25 18:29:32 by bkiskac          ###   ########.fr       */
+/*   Created: 2025/01/25 18:30:17 by bkiskac           #+#    #+#             */
+/*   Updated: 2025/01/25 18:31:04 by bkiskac          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../fractol.h"
 
-static void	events_init(t_fractol *fractol)
+int	clean_exit(t_fractol *fractol)
 {
-	mlx_hook(fractol->win, KeyPress, KeyPressMask, handle_key, fractol);
-	mlx_hook(fractol->win, ButtonPress, ButtonPressMask, handle_mouse, fractol);
-	mlx_hook(fractol->win, DestroyNotify, StructureNotifyMask, clean_exit, fractol);
+	mlx_destroy_image(fractol->mlx, fractol->img);
+	mlx_destroy_window(fractol->mlx, fractol->win);
+	mlx_destroy_display(fractol->mlx);
+	free(fractol->mlx);
+	exit(EXIT_SUCCESS);
 }
