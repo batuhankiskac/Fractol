346ececbf033f24b0ee5862d3b9fec5cfe3b0e4c
batuhankiskac <kiskacbatuhan@gmail.com>
2025-01-27 20:49:02 +0300

    Add fractol_set function to configure complex parameters for Julia set

--- src/fractol_render.c
+++ src/fractol_render.c
@@ -6,11 +6,25 @@
 /*   By: bkiskac <bkiskac@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 20:22:00 by bkiskac           #+#    #+#             */
-/*   Updated: 2025/01/27 20:24:51 by bkiskac          ###   ########.fr       */
+/*   Updated: 2025/01/27 20:30:13 by bkiskac          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../fractol.h"
+
+static void	fractol_set(t_fractal *z, t_fractal *c, t_fractal *fract)
+{
+	if (!ft_strncmp(fract->title, "Julia", 5))
+	{
+		c->compl_real = fract->julia_real;
+		c->compl_i = fract->julia_i;
+	}
+	else
+	{
+		c->compl_real = z->compl_real;
+		c->compl_i = z->compl_i;
+	}
+}
 
 void	fractol_render(t_fractal *fractal)
 {
