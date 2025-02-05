f9184bd8cb9041699a96b37a28b5816417b4677e
batuhankiskac <kiskacbatuhan@gmail.com>
2025-02-05 22:25:27 +0300

    Update color blending algorithm for improved rendering and consistency

--- src/fractol_color.c
+++ src/fractol_color.c
@@ -6,7 +6,7 @@
 /*   By: bkiskac <bkiskac@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 12:45:00 by bkiskac           #+#    #+#             */
-/*   Updated: 2025/01/31 17:28:23 by bkiskac          ###   ########.fr       */
+/*   Updated: 2025/02/05 22:04:10 by bkiskac          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,13 @@ int	blend_colors(int color1, int color2, double t)
 	int	b;
 
 	r = (int)((1 - t) * ((color1 >> 16) & 0xFF)
-			+ t * ((color2 >> 16) & 0xFF) * 5);
+			+ t * ((color2 >> 16) & 0xFF) * 3);
 	g = (int)((1 - t) * ((color1 >> 8) & 0xFF)
-			+ t * ((color2 >> 8) & 0xFF) * 5);
+			+ t * ((color2 >> 8) & 0xFF) * 3);
 	b = (int)((1 - t) * (color1 & 0xFF)
-			+ t * (color2 & 0xFF) * 5);
-	r = (int)(r + (255 - r) * t * 1.5);
-	g = (int)(g + (255 - g) * t * 0.2);
+			+ t * (color2 & 0xFF) * 3);
+	r = (int)(r + (255 - r) * t * 2);
+	g = (int)(g + (255 - g) * t * 0.1);
 	b = (int)(b + (255 - b) * t * 4);
 	return ((r << 16) | (g << 8) | b);
 }
